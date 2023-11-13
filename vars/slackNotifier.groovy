f6a6dcdbeabeb8ecf0e03dbd7df96ab4bbdf367c
Zoe Kodeer <zkodeer@gmail.com>
2023-11-13 19:33:39 +0300

    First commit

---
+++ vars/slackNotifier.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+
+def call(String buildResult) {
+  if ( buildResult == "SUCCESS" ) {
+    slackSend color: "good", message: "KODEER => CONGRATULATION: Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful ! more info ${env.BUILD_URL}"
+  }
+  else if( buildResult == "FAILURE" ) { 
+    slackSend color: "danger", message: "KODEER => BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed ! more info ${env.BUILD_URL}"
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    slackSend color: "warning", message: "KODEER => BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable ! more info ${env.BUILD_URL}"
+  }
+  else {
+    slackSend color: "danger", message: "KODEER => BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its result was unclear ! more info ${env.BUILD_URL}"	
+  }
+}
+
